8d0beac562302a846b5267eb3cd63a06095504c2
M4sq <wojciech.sycz@gmail.com>
2015-02-25 21:22:18 +0100

    Dodanie okna condInformation

---
+++ zadanie7_pp/zadanie7_pp/condInformation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zadanie7_pp
+{
+    /// <summary>
+    /// Interaction logic for condInformation.xaml
+    /// </summary>
+    public partial class condInformation : Window
+    {
+        public condInformation()
+        {
+            InitializeComponent();
+        }
+    }
+}
